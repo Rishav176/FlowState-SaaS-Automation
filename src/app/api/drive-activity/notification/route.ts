805a01b9aa0a400335c3ab24a748218552e197b9
Rishav Kumar <rishavkr1766@gmail.com>
2024-07-03 15:02:28 +0530

    chore: Add route for watching Google Drive changes and storing channel ID

--- src/app/api/drive-activity/notification/route.ts
+++ src/app/api/drive-activity/notification/route.ts
@@ -1,56 +1,147 @@
-import { db } from '@/lib/db'
-import { auth, clerkClient } from '@clerk/nextjs/server'
-import { google} from 'googleapis'
-import { NextResponse } from 'next/server'
-import { v4 as uuidv4 } from 'uuid'
-export async function GET() {
-    const oauth2Client = new google.auth.OAuth2(
-        process.env.GOOGLE_CLIENT_ID,
-        process.env.GOOGLE_CLIENT_SECRET,
-        process.env.GOOGLE_REDIRECT_URI
-    )
-    const {userId} = auth()
-    if (!userId) {
-        return NextResponse.json({message: 'User not found'})
-    }
-    const clerkResponse = await clerkClient.users.getUserOauthAccessToken(
-        userId,
-        'oauth_google'
-    )
-    
-    const accessToken = clerkResponse.data[0].token
-    oauth2Client.setCredentials({access_token: accessToken,})
+import { postContentToWebHook } from "@/app/(main)/(pages)/connections/_actions/discord-connection";
+import { onCreateNewPageInDatabase } from "@/app/(main)/(pages)/connections/_actions/notion-connection";
+import { postMessageToSlack } from "@/app/(main)/(pages)/connections/_actions/slack-connection";
+import { db } from "@/lib/db";
+import axios from "axios";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
-    const drive = google.drive({version: 'v3',auth: oauth2Client,})
-    const channelId= uuidv4()
-
-    const startPageTokenRes = await drive.changes.getStartPageToken()
-    const startPageToken = startPageTokenRes.data.startPageToken
-    if (startPageToken== null) {
-        throw new Error('startPageToken is unexpectedly null')
-    }
-    const listener = await drive.changes.watch({
-        pageToken: startPageToken,
-        supportsAllDrives: true,
-        supportsTeamDrives: true,
-        requestBody: {
-            id: channelId,
-            type: 'web_hook',
-            address: 'https://773e-106-222-217-197.ngrok-free.app/api/drive-activity/notification',
-            kind: 'api#channel',
+export async function POST(req: NextRequest) {
+    const headersList = headers()
+    let channelResourceId
+    headersList.forEach((value,key)=>{
+        if (key=='x-goog-resource-id'){
+            channelResourceId = value
         }
-    }) 
-    if (listener.status == 200){
-        const channelStored = await db.user.updateMany({
+    })
+
+    if (channelResourceId) {
+        const user = await db.user.findFirst({
             where: {
-                clerkId: userId,
+                googleResourceId: channelResourceId,
             },
-            data: {
-                googleResourceId: listener.data.resourceId,
-            },
+            select: {
+                clerkId: true, credits: true,
+            }
         })
-        if (channelStored) {
-            return new NextResponse('Listening to changes')
-        }
-    }
-}
+        if ((user && parseInt(user.credits!)>0)|| user?.credits == 'Unlimited'){
+            const workflow = await db.workflows.findMany({
+                where: {
+                    userId: user.clerkId,
+                }
+            })
+
+            if (workflow) {
+                workflow.map(async (flow) => {
+                    const flowPath = JSON.parse(flow.flowPath!)
+                    let current = 0
+                    while (current < flowPath.length){
+                        if (flowPath[current] == 'Discord') {
+                            const discordMessage = await db.discordWebhook.findFirst({
+                                where: {
+                                    userId: flow.userId,
+                                },
+                                select: {
+                                    url: true,
+                                },
+                            })
+                            if (discordMessage){
+                                await postContentToWebHook(
+                                    flow.discordTemplate!,discordMessage.url
+                                )
+                                flowPath.splice(flowPath[current],1)
+                            }
+                        }
+                        if (flowPath[current] == 'Slack') {
+                            const channels = flow.slackChannels.map((channel) => {
+                              return {
+                                label: '',
+                                value: channel,
+                              }
+                            })
+                            await postMessageToSlack(
+                              flow.slackAccessToken!,
+                              channels,
+                              flow.slackTemplate!
+                            )
+                            flowPath.splice(flowPath[current], 1)
+                          }
+                          if (flowPath[current] == 'Notion') {
+                            await onCreateNewPageInDatabase(
+                              flow.notionDbId!,
+                              flow.notionAccessToken!,
+                              JSON.parse(flow.notionTemplate!)
+                            )
+                            flowPath.splice(flowPath[current], 1)
+                          }
+                          if (flowPath[current] == 'Wait'){
+                            const res = await axios.put(
+                                'https://api.cron-job.org/jobs',
+                                {
+                                  job: {
+                                    url: `${process.env.NGROK_URI}?flow_id=${flow.id}`,
+                                    enabled: 'true',
+                                    schedule: {
+                                      timezone: 'Europe/Istanbul',
+                                      expiresAt: 0,
+                                      hours: [-1],
+                                      mdays: [-1],
+                                      minutes: ['*****'],
+                                      months: [-1],
+                                      wdays: [-1],
+                                    },
+                                  },
+                                },
+                                {
+                                  headers: {
+                                    Authorization: `Bearer ${process.env.CRON_JOB_KEY!}`,
+                                    'Content-Type': 'application/json',
+                                  },
+                                }
+                              )
+
+                              if (res) {
+                                flowPath.splice(flowPath[current], 1)
+                                const cronPath = await db.workflows.update({
+                                  where: {
+                                    id: flow.id,
+                                  },
+                                  data: {
+                                    cronPath: JSON.stringify(flowPath),
+                                  },
+                                })
+                                if (cronPath) break
+                              }
+                              break;
+                            }
+                            current++
+                    }
+                    await db.user.update({
+                        where: {
+                          clerkId: user.clerkId,
+                        },
+                        data: {
+                          credits: `${parseInt(user.credits!) - 1}`,
+                        },
+                      })      
+                })
+                return Response.json(
+                    {
+                      message: 'flow completed',
+                    },
+                    {
+                      status: 200,
+                    }
+                  )
+                }
+              }
+            }
+            return Response.json(
+              {
+                message: 'success',
+              },
+              {
+                status: 200,
+              }
+            )
+          }
