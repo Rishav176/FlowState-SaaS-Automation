34e8159eb24f3aa0492474d3f0999b5e3ec1e953
Rishav Kumar <rishavkr1766@gmail.com>
2024-08-04 20:43:38 -0600

    changed canvas sidebar element filtering

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
@@ -38,36 +38,41 @@ const EditorCanvasSidebar = ({nodes}: Props) => {
     },[nodeConnection])
 
 
-  return (
-    <aside>
-        <Tabs defaultValue ="actions" className="h-screen overflow-scroll pb-24">
-            <TabsList className="bg-transparent">
-                <TabsTrigger value="actions">Actions</TabsTrigger>
-                <TabsTrigger value="settings">Settings</TabsTrigger>
-            </TabsList>
-            <Separator/>
-        
-        <TabsContent value="actions" className="flex flex-col gap-4 p-4">
-            {Object.entries(EditorCanvasDefaultCardTypes).filter(
-                ([_, cardType])=> 
-                    (!nodes.length && cardType.type === 'Trigger') ||
-                    (nodes.length && cardType.type ==='Action')
-            )
-            .map(([cardKey, cardValue])=> (
-                <Card key={cardKey} draggable className='w-full cursur-grab border-black bg-neutral-100
-                dark:border-neutral-700 dark:bg-neutral-900' onDragStart={(event) => 
-                    onDragStart(event, cardKey as EditorCanvasTypes)
-                }>
-                    <CardHeader className="flex flex-row items-center gap-4 p-4">
-                        <EditorCanvasIconHelper type={cardKey as EditorCanvasTypes}/>
-                        <CardTitle className='text-md'>{cardKey}
-                            <CardDescription>{cardValue.description}</CardDescription>
-
-                        </CardTitle>
-                    </CardHeader>
-                </Card>
-            ))
-            }
+    return (
+        <aside>
+            <Tabs defaultValue="actions" className="h-screen overflow-scroll pb-24">
+                <TabsList className="bg-transparent">
+                    <TabsTrigger value="actions">Actions</TabsTrigger>
+                    <TabsTrigger value="settings">Settings</TabsTrigger>
+                </TabsList>
+                <Separator />
+            
+                <TabsContent value="actions" className="flex flex-col gap-4 p-4">
+                    {Object.entries(EditorCanvasDefaultCardTypes)
+                        .filter(([_, cardType]) => 
+                            (!nodes.length && cardType.type === 'Trigger') ||
+                            (nodes.length && cardType.type === 'Action')
+                        )
+                        .filter(([cardKey, _]) => 
+                            cardKey !== 'Action' && cardKey !== 'Condition' && cardKey !== 'Custom Webhook'
+                        )
+                        .map(([cardKey, cardValue]) => (
+                            <Card 
+                                key={cardKey} 
+                                draggable 
+                                className="w-full cursor-grab border-black bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-900"
+                                onDragStart={(event) => onDragStart(event, cardKey as EditorCanvasTypes)}
+                            >
+                                <CardHeader className="flex flex-row items-center gap-4 p-4">
+                                    <EditorCanvasIconHelper type={cardKey as EditorCanvasTypes} />
+                                    <CardTitle className="text-md">
+                                        {cardKey}
+                                        <CardDescription>{cardValue.description}</CardDescription>
+                                    </CardTitle>
+                                </CardHeader>
+                            </Card>
+                        ))
+                    }
 
         </TabsContent>
         <TabsContent value="settings" className='-mt-6'>
